e9dfb1d2bf4445360e431a7af9fc38abfd25c15b
Azimi2kht <s.h.azimidokht80@gmail.com>
2022-03-24 21:13:20 +0430

    Write a basic implementation of tcp-server (more detailed varsion will be added after implementation of tcp-client

---
+++ server/main.py
@@ -0,0 +1,17 @@
+from socket import *
+
+hostName = '127.0.0.1'
+port = 2121
+
+dataSocket = socket(AF_INET, SOCK_STREAM)
+controlSocket = socket(AF_INET, SOCK_STREAM)
+
+controlSocket.bind((hostName, port))
+
+controlSocket.listen(5)
+print("listening ...")
+
+while True:
+    connectionSock, address = controlSocket.accept()
+    msg = connectionSock.recv(1024).decode()
+    print("from client: ", address, " message: ", msg)
