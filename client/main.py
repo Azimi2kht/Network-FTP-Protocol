cf2af32cc38d3f9f99ad5449c361b8aef3e87336
Azimi2kht <s.h.azimidokht80@gmail.com>
2022-03-24 22:53:39 +0430

    Import ui module and add some functionality

--- client/main.py
+++ client/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from ui import *
+from socket import *
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+hostName = '127.0.0.1'
+serverPort = 2121
 
+# controlSocket = socket(AF_INET, SOCK_STREAM)
+# controlSocket.connect((hostName, serverPort))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    while True:
+        UI()
