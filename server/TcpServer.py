a30d947b1f26201e62a3ae7df8d2d82377c711e3
Azimi2kht <s.h.azimidokht80@gmail.com>
2022-03-31 08:24:24 +0430

    Fix the abstraction problem

--- server/TcpServer.py
+++ server/TcpServer.py
@@ -2,25 +2,36 @@ from socket import *
 
 
 class TcpServer:
-    def __init__(self, host_name: str, server_port: int):
-        self.sock = socket(AF_INET, SOCK_STREAM)
-        self.sock.bind((host_name, server_port))
-        self.sock.listen(5)
-        print("I'm listening ...")
-        self.client_connection, self.client_address = self.sock.accept()
-        print("connected to by address: ( ", self.client_address, " )")
+    def __init__(self, host_name=None, server_port=None, sock=None):
+        self.sock = None
+        if sock:
+            self.sock = sock
+        else:
+            self.sock = socket(AF_INET, SOCK_STREAM)
+            self.sock.bind((host_name, server_port))
+            self.sock.listen(5)
+            print("I'm listening ...")
+            temp, self.client_address = self.sock.accept()
+            self.connection = TcpServer(sock=temp)
+            print("connected to by address:`", self.client_address)
 
-    def get_client_connection(self):
-        return self.client_connection
+    def get_connection(self):
+        return self.connection
 
     def receive(self):
-        cl = self.get_client_connection()
-
-        message = self.client_connection.recv(1024).decode()  #
-        print(message)
+        """
+        :return: string ;the received data from the other end-system
+        """
+        message = self.connection.sock.recv(1024).decode()
         return message
 
     def send(self, message: str):
-        self.sock.send(message.encode())
+        """
+        :param message: string ;the message to be sent to the other end-system
+        """
+        self.connection.sock.send(message.encode())
 
-    # where to put the close function?!
+    def close(self):
+        self.sock.close()
+
+
