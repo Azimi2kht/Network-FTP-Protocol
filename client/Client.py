fa881c885f6a1d46e6595cf039c3b1b8f337410f
Azimi2kht <75935224+Azimi2kht@users.noreply.github.com>
2022-05-19 16:24:28 +0430

    Update Client.py

--- client/Client.py
+++ client/Client.py
@@ -13,16 +13,11 @@ class Client:
         self.server_port = server_port
         self.connection = FtpClient(host_name, server_port)
         self.help()
-        # self.connection.send_with_control("PWD")
-        # self.port_message = self.connection.receive_from_control()
-        # self.main_data = self.connection.receive_from_data(int(self.port_message))
 
     def get_command(self):
         command = input("\nEnter a command: ")
         if command.lower() == 'help':
             self.help()
-            # elif self.command == 'quit':
-        #  quit()
         elif command.lower() == 'list':
             self.list_files()
         elif command.lower() == 'pwd':
@@ -56,7 +51,6 @@ class Client:
         print(path)
 
     def change_directory(self, command: str):
-        # the following message doesn't make sense if the given path is invalid.
         print("\nchanging dir to: ", command[3:])
         print()
         self.connection.send_with_control(command)
@@ -87,26 +81,8 @@ class Client:
                 file.close()
                 data_socket.close()
                 print("\ndownloaded successfully.\n")
-        # self.connection.send_with_control(command)
-        # response = self.connection.receive_from_control()
-        # # check if the file exists.
-        # if response == FILE_UNAVAILABLE:
-        #     print('\nThe requested file does not exist in this directory.')
-        # else:
-        #     print('\nDownloading the file...')
-        #     # getting the data port:
-        #     data_port = int(self.connection.receive_from_control()) #ok
-        #     data = self.connection.receive_from_data(data_port).decode()
 
-        #     print(data)
-            
-        #     f = open(file_name, 'w')
-        #     f.write(data)
-        #     f.close()
-
-        #     print('\nFile downloaded.')
-
-
+                
 myClient = Client(HOST_NAME, PORT_NUMBER)
 while True:
     myClient.get_command()
